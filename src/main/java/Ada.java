36e7068e44975bd18411e2bd3cfbf3e72aeeaadf
Ke1sT <ke1stje01@gmail.com>
2026-01-20 14:51:14 +0800

    Rename, Greet, Exit

---
+++ src/main/java/Ada.java
@@ -0,0 +1,12 @@
+public class Ada {
+    public static void main(String[] args) {
+        
+        String WelcomeString = "____________________________________________________________\n"
+                + "Hello! I'm Ada\n"
+                + " What can I do for you?\n"
+                + "____________________________________________________________\n";
+        String GoodbyeString = "Bye. Hope to see you again soon!\n"
+                + "____________________________________________________________\n";
+        System.out.println(WelcomeString + GoodbyeString);
+    }
+}
